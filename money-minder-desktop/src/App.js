73326f9eb5568cc2391981328271e2fafc45685a
Sohil Jiwane <sohiljiwane@Sohils-MBP.lan>
2024-11-17 00:53:10 -1000

    Fixed rebase conflicts

--- money-minder-desktop/src/App.js
+++ money-minder-desktop/src/App.js
@@ -1,7 +1,5 @@
 import Buttons from "./components/ui/Buttons";
 import AppRoutes from "./components/util/AppRoutes";
-import Login from "./screens/login/Login";
-import Register from "./screens/register/Register";
 
 function App() {
   return (
