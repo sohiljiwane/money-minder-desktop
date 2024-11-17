5af6b61d2121503d9cace511c2617d7e9ff7c9c3
Sohil Jiwane <sohiljiwane@Sohils-MBP.lan>
2024-11-17 00:41:47 -1000

    Updated sidebar color schemes

--- money-minder-desktop/src/components/ui/Sidebar.tsx
+++ money-minder-desktop/src/components/ui/Sidebar.tsx
@@ -13,14 +13,14 @@ const Sidebar: React.FC<SidebarProps> = ({
 }) => {
   return (
     <div
-      className={`bg-squeezeGreen text-white h-screen p-4 ${
+      className={`bg-emerald-900 text-emerald-100 h-screen p-4 ${
         isCollapsed ? "w-16" : "w-64"
       } transition-width duration-300`}
     >
       <nav>
         <ul>
         <button
-        className="text-white focus:outline-none mb-6"
+        className="ext-emerald-200 hover:text-white focus:outline-none mb-6"
         onClick={toggleSidebar}
       >
         <i
@@ -33,9 +33,9 @@ const Sidebar: React.FC<SidebarProps> = ({
             <li key={index} className="mb-6">
               <a
                 href={link.path}
-                className="flex items-center space-x-2 hover:bg-darkGreen p-2 rounded"
+                className="flex items-center space-x-2 hhover:bg-emerald-800 hover:text-white p-2 rounded"
               >
-                <span className="text-lg">{link.icon}</span>
+                <span className="text-lg text-teal-300">{link.icon}</span>
                 {!isCollapsed && <span>{link.name}</span>}
               </a>
             </li>
